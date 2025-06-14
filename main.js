6aff8dab533543097532ee2fcac9ef4c14965f2c
myyonop <jeunsu813@gmail.com>
2025-06-14 21:48:41 +0900

    2

--- main.js
+++ main.js
@@ -20,3 +20,45 @@ let typing = function () {
 
 const typingInterval = setInterval(typing, 100);
 
+//--------------------
+
+const allHeaders = document.querySelectorAll(".section > h2");
+
+allHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const section = header.parentElement;
+    const contents = section.querySelectorAll(".content");
+
+    contents.forEach(content => {
+      content.classList.toggle("visible");
+    });
+  });
+});
+
+
+//--------------------
+
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(acc => {
+  acc.querySelector('.accordion-title').addEventListener('click', () => {
+    // 모두 닫기
+    accordions.forEach(a => a.classList.remove('open'));
+
+    // 누른 것만 열기
+    acc.classList.toggle('open');
+  });
+});
+
+
+
+document.querySelectorAll(".fade-table").forEach(table => {
+  const rows = [ ...table.querySelectorAll("thead tr, tbody tr") ];
+  rows.forEach((row, i) => {
+    setTimeout(() => {
+      row.classList.add("visible");
+    }, i * 300); // 또는 trigger로 연결해도 됨
+  });
+});
+
+
